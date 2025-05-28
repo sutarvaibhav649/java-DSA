393b9c54d76b9354d27e1144b69a5d21b566897b
sutarvaibhav649 <sutar.vaibhav649@gmail.com>
2025-05-28 19:33:06 +0530

    modify stack array

--- stack/StackArray.java
+++ stack/StackArray.java
@@ -92,6 +92,8 @@ public class StackArray {
                     System.out.println("❌ Invalid option try again ❌");
                 }
             }
+            scan.close();
         }
+
     }
 }
